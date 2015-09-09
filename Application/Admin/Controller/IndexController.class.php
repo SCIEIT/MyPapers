86f8f726d69651acad8da5207af5ca5b07c674b0
HolmesWu <wuyueholmes@qq.com>
2015-09-09 20:14:03 +0800

    添加压缩文件处理工具

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,15 +1,10 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-#!/usr/bin/perl
-use cPanelUserConfig;
+use Common\Extensions\clsTbsZip;
 class IndexController extends BaseController {
     public function _initialize(){
     	header("Content-type: text/html;charset=utf-8");
-    }
-    public function index(){
-    	$hello=new Hello();
-    	$hello->sayHello();
     }
     public function initpapers(){
    		$PapersArr=$this->readDir('./Papers_DIR/unpacked');
@@ -23,21 +18,66 @@ class IndexController extends BaseController {
     }
     public function packpapers(){
         $subjects=D('subjects')->select();
+        $paperArr=[];
+        $countArr=[];
         foreach ($subjects as $subject) {
-            $zip=new ZipArchive();
-                if($zip->open('./Papers_DIR/packed/'.$subject['subject_code'].'.zip',ZipArchive::OVERWRITE)===TRUE){
-                echo '成功创建:'.$subject['subject_code'].'.zip--'.$subject['subject_name'].'<br/>';
-                $papers=D('papers')->where(['subject_code'=>$subject['subject_code']])->getFeild('paper_name');
+            $code=$subject['subject_code'];
+             $paperArr[$code]=D('papers')->where(['subject_code'=>$subject['subject_code']])->getField('paper_name',true);
+             $countArr[$code]=D('papers')->where(['subject_code'=>$subject['subject_code']])->count();
+        }
+        foreach ($subjects as $subject) {
+            if(!file_exists('./Papers_DIR/packed/'.$subject['subject_code'].'.zip')){
+                $zip = new clsTbsZip();
+                $zip->CreateNew();
+            // $zip=new ZipArchive();
+            //     if($zip->open('./Papers_DIR/packed/'.$subject['subject_code'].'.zip',ZipArchive::OVERWRITE)===TRUE){
+                echo '正在创建:'.$subject['subject_code'].'.zip--'.$subject['subject_name'].'<br/>';
+                $papers=$paperArr[$subject['subject_code']];
+                $filearr=[];
+                ob_flush();
+                flush();
                 foreach ($papers as $paper) {
-                    $zip->addFile('./Papers_DIR/unpacked/'.$paper);
-                    echo '.';
+                    $zip->FileAdd($paper, './Papers_DIR/unpacked/'.$paper, TBSZIP_FILE);
+                    echo '添加数据：'.$paper.'<br/>';
                     ob_flush();
                     flush();
                 }
-                $zip->close();
-                echo '完成添加数据。<br/>';
+                // $zip->close();
+                echo '完成添加数据，正在保存。。。。<br/>';
                 ob_flush();
                 flush();
+                $zip->Flush(TBSZIP_FILE, './Papers_DIR/packed/'.$subject['subject_code'].'tmp.zip'); // apply modifications as a new local file
+                echo '保存完毕<br/>';
+                rename('./Papers_DIR/packed/'.$subject['subject_code'].'tmp.zip','./Papers_DIR/packed/'.$subject['subject_code'].'.zip');
+                ob_flush();
+                flush();
+                $zip->close();
+            // }
+            }else{
+                echo '正在检查 '.$subject['subject_code'].'.zip 的数据完整性。<br/>';
+                $zip = new clsTbsZip();
+                $zip->Open('./Papers_DIR/packed/'.$subject['subject_code'].'.zip');
+                ob_flush();
+                flush();
+                $papers=$paperArr[$subject['subject_code']];
+                if($countArr[$subject['subject_code']]!=count($zip->CdFileLst)){
+                    echo $countArr[$subject['subject_code']].'::'.count($zip->CdFileLst).'<br/>';
+                    $zip->CreateNew();
+                    foreach ($papers as $paper) {
+                        $zip->FileAdd($paper, './Papers_DIR/unpacked/'.$paper, TBSZIP_FILE);
+                        echo '添加数据：'.$paper.'<br/>';
+                        ob_flush();
+                        flush();
+                    }
+                    $zip->Flush(TBSZIP_FILE, './Papers_DIR/packed/'.$subject['subject_code'].'tmp.zip');
+                    $zip->close();
+                    @unlink ('./Papers_DIR/packed/'.$subject['subject_code'].'.zip');
+                    rename('./Papers_DIR/packed/'.$subject['subject_code'].'tmp.zip','./Papers_DIR/packed/'.$subject['subject_code'].'.zip');
+                }else{
+                    echo '通过检验。<br/>';
+                    $zip->close();
+                }
+                
             }
         }
     }
