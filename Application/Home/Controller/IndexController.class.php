775be5b305f62e6fb475ca4bf5b3e7352fa65bef
HolmesWu <wuyueholmes@qq.com>
2015-12-17 19:40:58 +0800

    Skip landing

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends BaseController {
     public function index(){
+    	if(isset($_COOKIE['grade'])){
+    		redirect(U('list/catebase'));
+    	}
     	$this->initialize('MyPapers');
     	$this->display();
     }
