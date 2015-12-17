58724f5b725b286dc6ebdea6587387009d111c62
HolmesWu <wuyueholmes@qq.com>
2015-12-17 19:34:19 +0800

    完善Reader及周边程序，将crone作业

--- SmartEngine/Run.py
+++ SmartEngine/Run.py
@@ -4,6 +4,8 @@ import Zip
 if __name__ == '__main__':
     reader=Reader.Reader()
     (errarr,undifinedlist,newlist)=reader.run()
+    if(len(undifinedlist)>0):
+        reader.addField(undifinedlist)
     if len(newlist)>0:
         compresser=Zip.Zip()
         compresser.run(newlist)
